c7a3be0aded435c6cacff7c934b84fe85ea272c2
Jeff Lyon <jeff@albatrossdigital.com>
2015-11-08 11:38:38 -0800

    Adding gps, distance calculation

--- www/js/app.js
+++ www/js/app.js
@@ -1,4 +1,4 @@
-angular.module('starter', ['ionic', 'starter.services'])
+angular.module('starter', ['ionic', 'starter.services', 'ngCordova'])
 
 .config(function($stateProvider, $urlRouterProvider) {
 
@@ -57,15 +57,46 @@ angular.module('starter', ['ionic', 'starter.services'])
     $scope.trips = Data.getAll({id: "1"});
 })
 
-.controller('ActiveTabCtrl', function($scope, $state, Data) { 
+.controller('ActiveTabCtrl', function($scope, $state, Data, $cordovaGeolocation) { 
   $scope.totalMiles = 0;
   $scope.mpg = window.localStorage['mpg'];
   var counter = setInterval(gps, 1000);
+
+  var watch = $cordovaGeolocation.watchPosition({ frequency: 1000 });
+  watch.then(function() { /* Not  used */ }, 
+    function(err) {
+      // An error occurred.
+    }, 
+    function(position) {
+      // Active updates of the position here
+      // position.coords.[ latitude / longitude]
+     // this is where you will add your code to track changes in co-ordinates
+     $scope.lat = position.coords.latitude;
+     alert(position.coords.latitude);
+  });
 
   function gps(){
     $scope.totalMiles ++;
     $scope.carbon = (19.64/$scope.mpg) * $scope.totalMiles;
     $scope.$apply();
+  }
+
+  function dist(lat1, lon1, lat2, lon2) {
+    // Distance haversine formula from http://www.movable-type.co.uk/scripts/latlong.html.
+    var R = 6371000; // metres
+    var φ1 = lat1.toRadians();
+    var φ2 = lat2.toRadians();
+    var Δφ = (lat2-lat1).toRadians();
+    var Δλ = (lon2-lon1).toRadians();
+
+    var a = Math.sin(Δφ/2) * Math.sin(Δφ/2) +
+            Math.cos(φ1) * Math.cos(φ2) *
+            Math.sin(Δλ/2) * Math.sin(Δλ/2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+
+    var d = R * c; // meters
+    d = d * 0.00062137; // miles
+    return d;
   }
   
   $scope.stopClick = function(){
